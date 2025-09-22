512e5c7cb0cb53cace4cbeb69dba70aa2797f36a
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-09-23 02:07:40 +0800

    Only compute area error after normalization

--- src/inset_state/integrate.cpp
+++ src/inset_state/integrate.cpp
@@ -56,15 +56,15 @@ void InsetState::prepare_for_integration()
   }
 
   // Store initial inset area to calculate area drift,
-  // set area errors based on this initial_area
   store_initial_area();
-  set_area_errors();
 
   // Store initial target area to normalize inset areas
   store_initial_target_area();
 
   // Normalize total target area to be equal to initial area
   normalize_target_area();
+
+  set_area_errors();
 }
 
 void InsetState::cleanup_after_integration()
